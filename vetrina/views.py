454b69e881218fd18785c57f06f2bcec4bf13cf2
superqwe <superqwe@users.noreply.github.com>
2017-10-02 09:14:07 +0200

    creata struttura base template

--- vetrina/views.py
+++ vetrina/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from pprint import pprint as pp
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the vetrina index.")
+    return render_to_response('vetrina/index.html', locals(), )
