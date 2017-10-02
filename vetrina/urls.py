5df292c21ffe3b0780e7f80fb747aeeb9f4288fd
superqwe <superqwe@users.noreply.github.com>
2017-10-02 18:20:32 +0200

    - aggoirnato urlpattern - create viste alle pagine

--- vetrina/urls.py
+++ vetrina/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
+    url(r'collezioni/$', views.collezioni, name='collezioni'),
+    url(r'eventi/$', views.eventi, name='eventi'),
+    url(r'contatti/$', views.contatti, name='contatti'),
+    url(r'about/$', views.about, name='about'),
 ]
--- vetrina/views.py
+++ vetrina/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from pprint import pprint as pp
 
-def index(request):
-    lara = 'Lara BjYou'
-    return render_to_response('vetrina/index.html', locals(), )
+LARA =  'Lara BjYou'
+
+def home(request):
+    lara = LARA
+    return render_to_response('vetrina/home.html', locals(), )
+
+def collezioni(request):
+    lara = LARA
+    return render_to_response('vetrina/home.html', locals(), )
+
+def eventi(request):
+    lara = LARA
+    return render_to_response('vetrina/home.html', locals(), )
+
+def contatti(request):
+    lara = LARA
+    return render_to_response('vetrina/home.html', locals(), )
+
+def about(request):
+    lara = LARA
+    return render_to_response('vetrina/home.html', locals(), )
+
