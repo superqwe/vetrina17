7c2b8908045e0bdacf563537c352e1dfb9e0cde2
superqwe <superqwe@users.noreply.github.com>
2017-10-02 10:20:54 +0200

    bozza nav

--- vetrina/views.py
+++ vetrina/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from pprint import pprint as pp
 
 def index(request):
-
+    lara = 'Lara BjYou'
     return render_to_response('vetrina/index.html', locals(), )
