19b45493ac61a67836d9a756116546753bb639ac
superqwe <superqwe@users.noreply.github.com>
2017-10-05 11:50:32 +0200

    messaggio salvato su db

---
+++ vetrina/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from .models import Messaggio
+
+
+class MessaggioForm(ModelForm):
+    class Meta:
+        model = Messaggio
+        fields = ['nome', 'email', 'messaggio']
+
+
--- vetrina/views.py
+++ vetrina/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 from django.http import HttpResponse
 from pprint import pprint as pp
 
 import vetrina.services as services
+from vetrina.forms import MessaggioForm
 
-LARA =  'Lara BjYou'
+LARA = 'Lara BjYou'
+
 
 def home(request):
     lara = LARA
     nav_attivo = 'home'
 
     return render_to_response('vetrina/home.html', locals(), )
+
 
 def collezioni(request, tipo=None, anno=None):
     lara = LARA
@@ -28,15 +31,36 @@ def eventi(request):
 
     return render_to_response('vetrina/eventi.html', locals(), )
 
+
 def contatti(request):
     lara = LARA
     nav_attivo = 'contatti'
+    messaggio_inviato = False
 
-    return render_to_response('vetrina/contatti.html', locals(), )
+    if request.method == 'POST':
+        form_messaggio = MessaggioForm(request.POST)
+
+        if form_messaggio.is_valid():
+            messaggio = form_messaggio.save(commit=False)
+            messaggio.nome = request.POST['nome']
+            messaggio.email = request.POST['email']
+            messaggio.messaggio = request.POST['messaggio']
+            messaggio.save()
+
+            messaggio_inviato = True
+
+    else:
+        form_messaggio = MessaggioForm()
+
+    return render(request, 'vetrina/contatti.html',
+                  {'form_messaggio': form_messaggio, 'nav_attivo': nav_attivo, 'lara': lara,
+                   'messaggio_inviato': messaggio_inviato})
+
+    # return render_to_response('vetrina/contatti.html', locals(), )
+
 
 def about(request):
     lara = LARA
     nav_attivo = 'about'
 
     return render_to_response('vetrina/about.html', locals(), )
-
